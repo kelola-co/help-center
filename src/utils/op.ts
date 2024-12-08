f053c779edca048a6909ea2c141f862aa6548112
Adjie Purbojati <official.purbojati@gmail.com>
2024-12-08 19:21:24 +0700

    Add analytics

---
+++ src/utils/op.ts
@@ -0,0 +1,8 @@
+import { OpenPanel } from '@openpanel/web';
+ 
+export const op = new OpenPanel({
+  clientId: 'f7be3664-8c25-41b2-b934-30bea0ed07b9',
+  trackScreenViews: true,
+  trackOutgoingLinks: true,
+  trackAttributes: true,
+});
